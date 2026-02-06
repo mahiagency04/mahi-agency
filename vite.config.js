02e30ef147dea968f8ea40ac5693dae318d7080f
mahiagency04 <mahiagency04@gmail.com>
2026-02-06 10:20:11 +0530

    Added sitemap

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import sitemap from 'vite-plugin-sitemap'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    sitemap({
+      hostname: 'https://www.mahiagency.in'
+    })
+  ],
 })
 
