9b8fad5e849ebfb07d2585adaea85582827e8f5e
mahiagency04 <mahiagency04@gmail.com>
2026-03-05 10:32:39 +0530

    fixed sitemap plugin

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,38 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import sitemap from 'vite-plugin-sitemap'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     sitemap({
-      hostname: 'https://www.mahiagency.in'
+      hostname: 'https://www.mahiagency.in',
+      robots: [
+        {
+          userAgent: '*',
+          allow: '/',
+        }
+      ]
     })
   ],
 })
+
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react-swc'
+// import sitemap from 'vite-plugin-sitemap'
+
+// https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [
+//     react(),
+//     sitemap({
+//       hostname: 'https://www.mahiagency.in'
+//       robots: [
+//         {
+//           userAgent: '*',
+//           allow: '/',
+//         }
+//       ]
+//     })
+//   ],
+// })
 
